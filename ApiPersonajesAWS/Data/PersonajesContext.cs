c576942edac1bc98088bdb19976a49fe88deb419
Barby <114255540+barjimen@users.noreply.github.com>
2025-05-13 11:11:10 +0200

    add+

--- ApiPersonajesAWS/Data/PersonajesContext.cs
+++ ApiPersonajesAWS/Data/PersonajesContext.cs
@@ -5,7 +5,7 @@ namespace ApiPersonajesAWS.Data
 {
     public class PersonajesContext : DbContext
     {
-        public PersonajesContext(DbContextOptions<PersonajesContext> options) : base()
+        public PersonajesContext(DbContextOptions<PersonajesContext> options) : base(options)
         { }
         public DbSet<Personaje> Personajes { get; set; }
     }
